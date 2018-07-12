7dec7cbe513ca04f327a4bd4f2818eec1ca39de2
Tasuja <sujata@vayu.com.au>
2018-07-12 15:25:21 +0545

    changed color

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this, too</p>
+                <p v-highlight:background.delayed="'coral'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'grey', secondColor: 'coral', delay: 500}">Color this, too</p>
             </div>
         </div>
     </div>
